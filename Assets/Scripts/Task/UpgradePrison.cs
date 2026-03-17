f2d3e7f7a364604afbd2a2573992367d7122d7db
shd <masterseo01@naver.com>
2026-03-17 13:19:46 +0900

    [mod] NavMesh 관리 범위를 맵 전체에서 tilemap으로 변경

---
+++ Assets/Scripts/Task/UpgradePrison.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpgradePrison : MonoBehaviour
+{
+    [SerializeField]private PrisonerManager prisonerManager;
+    private void OnTriggerEnter(Collider other)
+    {
+        prisonerManager.ExpandPrisonArea(prisonerManager.CurruntTileCount);
+    }
+}
